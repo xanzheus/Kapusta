1935dea9996ec455813148a4171a48c26160a696
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-12-16 15:03:54 +0200

    files added

--- src/components/Statistic/GraphDetails/Categories/Categories.jsx
+++ src/components/Statistic/GraphDetails/Categories/Categories.jsx
@@ -1,39 +1,129 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import s from './Categories.module.css';
+import sprite from '../../../../images/svg/sprite.svg';
+import s from './Categories.module.scss';
 
-const Categories = ({ updateData }) => {
-  const [radioButton, setRadioButton] = useState(true);
+const Categories = ({ updateData, setActiveCalss }) => {
+  const [value, setValue] = useState('РАСХОДЫ');
+  const [selectedCategory, setselectedCategory] = useState('');
+  const [isActive, setIsActive] = useState(false);
+
+  // категории РЫБА доходы
+  const categories = [
+    { name: 'Продукты', id: 1, amount: 5000.0 },
+    { name: 'Алкоголь', id: 2, amount: 200.0 },
+    { name: 'Развлечения', id: 3, amount: 800.0 },
+    { name: 'Здоровье', id: 4, amount: 900.0 },
+    { name: 'Транспорт', id: 5, amount: 2000.0 },
+    { name: 'Коммуналка, связь', id: 6, amount: 1500.0 },
+    { name: 'Всё для дома', id: 7, amount: 1500.0 },
+    { name: 'Образование', id: 8, amount: 2400.0 },
+    { name: 'Прочее', id: 9, amount: 12000.06 },
+  ];
+
+  // категории РЫБА расходы
+  const categoriesIncome = [
+    { name: 'ЗП', id: 1, amount: 5000.0 },
+    { name: 'Доп.доход', id: 2, amount: 200.0 },
+  ];
+
+  const resultValue = () => {
+    if (value === 'РАСХОДЫ') {
+      setValue('ДОХОДЫ');
+    }
+    if (value === 'ДОХОДЫ') {
+      setValue('РАСХОДЫ');
+    }
+  };
 
   return (
     <div className={s.categories}>
-      <FormControl component="fieldset">
-        {/* <FormLabel component="legend"></FormLabel> */}
-        <RadioGroup row aria-label="balance" name="row-radio-buttons-group" defaultValue="costs">
-          <FormControlLabel
-            value="costs"
-            control={<Radio />}
-            label="Расходы"
-            onChange={() => {
-              setRadioButton(true);
-              updateData(radioButton);
-            }}
-          />
-          <FormControlLabel
-            value="incoming"
-            control={<Radio />}
-            label="Доходы"
-            onChange={() => {
-              setRadioButton(false);
-              updateData(radioButton);
-            }}
-          />
-        </RadioGroup>
-      </FormControl>
+      <div className={s.categories__select}>
+        <button
+          className={s.search__buttonPickerBack}
+          onClick={() => {
+            resultValue();
+          }}
+        ></button>
+        <div
+          onChange={() => {
+            updateData(value);
+          }}
+          className={s.categories__currentValue}
+        >
+          {value}
+        </div>
+
+        <button
+          className={s.search__buttonPickerNext}
+          onClick={() => {
+            resultValue();
+          }}
+        ></button>
+      </div>
+
+      {value === 'РАСХОДЫ' && (
+        <div className="income">
+          <div className={s.categories__listItems}>
+            <ul className={s.categorie__list}>
+              {categories.map(item => {
+                return (
+                  <li
+                    className={s.categorie__items}
+                    key={item.name}
+                    onClick={() => {
+                      setselectedCategory(item.name);
+                      console.log(selectedCategory);
+                      setIsActive(true);
+                      setActiveCalss(true);
+                    }}
+                  >
+                    {item.amount}
+                    <a>
+                      {/* <svg className={isActive ? s.icon : s.iconActive}> */}
+                      <svg className={s.icon}>
+                        <use xlinkHref={`${sprite}#${item.name}`} />
+                      </svg>
+                    </a>
+                    <p className={s.categorie__name}>{item.name}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {value === 'ДОХОДЫ' && (
+        <div className="costs">
+          <div className={s.categories__listItems}>
+            <ul className={s.categorie__list}>
+              {categoriesIncome.map(item => {
+                return (
+                  <li
+                    className={s.categorie__items}
+                    key={item.name}
+                    onClick={() => {
+                      setselectedCategory(item.name);
+                      console.log(selectedCategory);
+                      setIsActive(true);
+                      setActiveCalss(true);
+                    }}
+                  >
+                    {item.amount}
+                    <a>
+                      <svg className={s.icon}>
+                        <use xlinkHref={`${sprite}#${item.name}`} />
+                      </svg>
+                    </a>
+                    <p className={s.categorie__name}>{item.name}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Statistic/StatHeader/BackButton/BackButton.jsx
+++ src/components/Statistic/StatHeader/BackButton/BackButton.jsx
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
-// import Button from '@mui/material/Button';
-import s from './BackButton.module.css';
+import s from './BackButton.module.scss';
 
 const BackButton = () => {
   return (
-    <div>
-      {/* <Button variant="outlined">Вернуться на главную</Button> */}
-      <Link to="/">Вернуться на главную</Link>
+    <div className={s.backButton}>
+      <Link to="/">
+        <div className={s.backButton__block}>
+          {' '}
+          <svg
+            width="18"
+            height="12"
+            viewBox="0 0 18 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={s.backButtonIcon}
+          >
+            <path d="M18 5H3.83L7.41 1.41L6 0L0 6L6 12L7.41 10.59L3.83 7H18V5Z" fill="#FF751D" />
+          </svg>
+          <p className={s.backButton__text}>Вернуться на главную</p>
+        </div>
+      </Link>
     </div>
   );
 };
