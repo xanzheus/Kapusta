2fe70230805de1b1cfe49a07ecc2d112ea029eb6
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-12-17 16:36:25 +0200

    fix path COLORSS

--- src/Constants/index.jsx
+++ src/Constants/index.jsx
@@ -1,4 +1,4 @@
 export { default as LANGUAGE } from './language';
 export { default as CURRENCY } from './currency';
 export { default as THEME } from './theme';
-export { default as COLORS } from './colors';
+export { default as COLORS } from './COLORSS';
