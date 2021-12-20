d3fcbe25f0eafd3f758ddf44e7d285e0f7e5b470
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-12-21 00:33:16 +0200

    fix logout

--- src/redux/service/userAPI.jsx
+++ src/redux/service/userAPI.jsx
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from './authSlice';
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://adamants-wallet-project-back.herokuapp.com/api/users/',
-  credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.accessToken;
     console.log('header', token);
@@ -62,18 +61,16 @@ export const userAPI = createApi({
           email,
           password,
         },
-        credentials: 'include',
       }),
     }),
 
-    logout: builder.query({
+    logout: builder.mutation({
       query: () => ({
         url: '/logout',
-        method: 'GET',
+        method: 'POST',
         headers: {
           authorization: '',
         },
-        credentials: 'include',
       }),
     }),
 
@@ -123,7 +120,7 @@ export const userAPI = createApi({
 export const {
   useCreateUserMutation,
   useLoginMutation,
-  useLogoutQuery,
+  useLogoutMutation,
   useGetDataUserQuery,
   useUpdateAvatarMutation,
   useInviteFriendMutation,
