1fb32ff457ce0734032367f8f679abf92cf07532
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-12-21 05:33:57 +0200

    adds snow

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import UserMenu from 'components/UserMenu';
 import Container from 'components/Container';
 import { Link } from 'react-router-dom';
-// import { useGetCurrentUserQuery } from 'redux/service/currentUserAPI';
+import { IconButton } from '@mui/material';
 import { useSelector } from 'react-redux';
+import Snowfall from 'react-snowfall';
+import Snowflakes from 'magic-snowflakes';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 import style from './AppBar.module.scss';
 
 const AppBar = () => {
+  // const [snowVisibility, setSnowVisibility] = useState(false);
+  const snowflakes = new Snowflakes({ height: 40, speed: 0.2, count: 16 });
+
+  true && snowflakes.start();
+
+  // const [showPassword, setShowPassword] = useState(false);
   const accessToken = useSelector(state => state.auth.accessToken);
+  // const handleClickSnow = () => {
+  //   console.log(snowVisibility);
+  //   setSnowVisibility(!snowVisibility);
+  // };
 
   // const { data } = useGetCurrentUserQuery();
   return (
