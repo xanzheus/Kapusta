161d6383b9953cbbe588e29a6aa8386c625c6e88
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-12-23 01:18:38 +0200

    fix Prop Types

--- src/components/Balance/HeaderTabs/HeaderTabs.jsx
+++ src/components/Balance/HeaderTabs/HeaderTabs.jsx
@@ -54,13 +54,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const HeaderTabs = ({
-  getCurrentDate,
-  transactions,
-  incomReportData,
-  expensesReportData,
-  initialDate,
-}) => {
+const HeaderTabs = ({ getCurrentDate, transactions, initialDate }) => {
   const [value, setValue] = useState('1');
 
   const refreshedTransactions = (symbol, type) => {
@@ -119,7 +113,6 @@ const HeaderTabs = ({
             initialDate={initialDate}
             Class="expenses"
             data={refreshedTransactions('-', CATEGORYTYPE.EXPENSE)}
-            // reportData={expensesReportData}
             type={CATEGORYTYPE.EXPENSE}
             category={expensesCatagoryArray}
           />
@@ -138,7 +131,6 @@ const HeaderTabs = ({
             initialDate={initialDate}
             Class="income"
             data={refreshedTransactions('', CATEGORYTYPE.INCOME)}
-            // reportData={incomReportData}
             category={incomeCatagoryArray}
             type={CATEGORYTYPE.INCOME}
           />
@@ -150,8 +142,6 @@ const HeaderTabs = ({
 
 HeaderTabs.propTypes = {
   getCurrentDate: PropTypes.func.isRequired,
-  incomReportData: PropTypes.array.isRequired,
-  expensesReportData: PropTypes.array.isRequired,
   transactions: PropTypes.array.isRequired,
   initialDate: PropTypes.object.isRequired,
 };
