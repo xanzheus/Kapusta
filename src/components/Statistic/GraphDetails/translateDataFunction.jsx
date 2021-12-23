bc05d8f197bccab568945f083cb3f926be24f618
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-12-23 16:02:43 +0200

    return warning fixed

--- src/components/Statistic/GraphDetails/translateDataFunction.jsx
+++ src/components/Statistic/GraphDetails/translateDataFunction.jsx
@@ -1,46 +1,48 @@
-  const dataTranslated = val => {
-    const newOb = val.map(i => {
-      if (i.category === 'products') {
-        return { ...i, category: 'Продукты' };
-      }
-      if (i.category === 'home') {
-        return { ...i, category: 'Всё для дома' };
-      }
-      if (i.category === 'entertainment') {
-        return { ...i, category: 'Развлечения' };
-      }
-      if (i.category === 'healthy') {
-        return { ...i, category: 'Здоровье' };
-      }
-      if (i.category === 'transport') {
-        return { ...i, category: 'Транспорт' };
-      }
-      if (i.category === 'alcohol') {
-        return { ...i, category: 'Алкоголь' };
-      }
-      if (i.category === 'technic') {
-        return { ...i, category: 'Техника' };
-      }
-      if (i.category === 'communication') {
-        return { ...i, category: 'Коммуналка, связь' };
-      }
-      if (i.category === 'hobby') {
-        return { ...i, category: 'Спорт, хобби' };
-      }
-      if (i.category === 'education') {
-        return { ...i, category: 'Образование' };
-      }
-      if (i.category === 'other') {
-        return { ...i, category: 'Прочее' };
-      }
-      if (i.category === 'salary') {
-        return { ...i, category: 'ЗП' };
-      }
-      if (i.category === 'additional') {
-        return { ...i, category: 'Доп.доход' };
-      }
-    });
-    return newOb;
-  };
+const dataTranslated = val => {
+  const newOb = val.map(i => {
+    if (i.category === 'products') {
+      return { ...i, category: 'Продукты' };
+    }
+    if (i.category === 'home') {
+      return { ...i, category: 'Всё для дома' };
+    }
+    if (i.category === 'entertainment') {
+      return { ...i, category: 'Развлечения' };
+    }
+    if (i.category === 'healthy') {
+      return { ...i, category: 'Здоровье' };
+    }
+    if (i.category === 'transport') {
+      return { ...i, category: 'Транспорт' };
+    }
+    if (i.category === 'alcohol') {
+      return { ...i, category: 'Алкоголь' };
+    }
+    if (i.category === 'technic') {
+      return { ...i, category: 'Техника' };
+    }
+    if (i.category === 'communication') {
+      return { ...i, category: 'Коммуналка, связь' };
+    }
+    if (i.category === 'hobby') {
+      return { ...i, category: 'Спорт, хобби' };
+    }
+    if (i.category === 'education') {
+      return { ...i, category: 'Образование' };
+    }
+    if (i.category === 'other') {
+      return { ...i, category: 'Прочее' };
+    }
+    if (i.category === 'salary') {
+      return { ...i, category: 'ЗП' };
+    }
+    if (i.category === 'additional') {
+      return { ...i, category: 'Доп.доход' };
+    }
+    return null;
+  });
+  console.log(newOb);
+  return newOb;
+};
 
-  export default dataTranslated;
+export default dataTranslated;
