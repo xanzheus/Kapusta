b76a0409b083e5371c4c3cb4a4c4ac804755fd3c
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-12-27 23:29:40 +0200

    Update MobileOverlayToTransactions.jsx

--- src/components/Balance/Mobile/TranceActions/MobileOverlayToTransactions.jsx
+++ src/components/Balance/Mobile/TranceActions/MobileOverlayToTransactions.jsx
@@ -2,7 +2,7 @@ import Stack from '@mui/material/Stack';
 import COLORS from 'Constants/COLORS';
 
 const MobileOverlayToTransactions = () => (
-  <Stack textAlign="center" mt={10} fontWeight={700} color={COLORS.mainAccent}>
+  <Stack textAlign="center" mt={8} fontWeight={700} color={COLORS.mainAccent}>
     Нет транзакций!
   </Stack>
 );
